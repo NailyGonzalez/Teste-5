c66a03b5651f308869d9435823a684a814bef59e
NailyGonzalez <62552085+NailyGonzalez@users.noreply.github.com>
2025-08-25 00:51:48 -0300

    package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "teste-railway",
+  "version": "1.0.0",
+  "description": "Teste mínimo para rodar no Railway",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
